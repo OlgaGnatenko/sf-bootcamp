9cd31013d97dd84e5608499771ec8c1994cea031
Olga Gnatenko <olgagnat13@gmail.com>
2021-04-07 05:47:23 +0300

    feat: add positions list

--- force-app/main/default/lwc/positionsList/positionsList.js
+++ force-app/main/default/lwc/positionsList/positionsList.js
@@ -1,3 +1,32 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from "lwc";
 
-export default class PositionsList extends LightningElement {}
+import getJobPositions from "@salesforce/apex/JobPositionController.getJobPositions";
+const COLUMNS = [
+  { label: "Name", fieldName: "Job_Position__c.Name", type: "text" },
+  {
+    label: "Title",
+    fieldName: "Job_Position__c.Job_Position_Title__c",
+    type: "text"
+  },
+  {
+    label: "Candidate Level",
+    fieldName: "Job_Position__c.Candidate_Level__c",
+    type: "text"
+  },
+  {
+    label: "Practice Unit",
+    fieldName: "Job_Position__c.Practice_Unit__c",
+    type: "text"
+  },
+  { label: "Location", fieldName: "Job_Position__c.Location__c", type: "text" },
+  {
+    label: "Position Budget",
+    fieldName: "Job_Position__c.Position_Budget__c",
+    type: "currency"
+  }
+];
+export default class PositionsList extends LightningElement {
+  columns = COLUMNS;
+  @wire(getJobPositions)
+  positions;
+}
